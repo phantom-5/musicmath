b2534be4d1de68ff4f21e2c4e6de40740bbccccc
Rudra Madhav Biswal <35427300+phantom-5@users.noreply.github.com>
2020-10-01 06:57:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function custom(){
 }
 
 function showInstr(){
-    text='1.Supports expressions containing *,^,/,%,sin,cos,tan and many more ...(https://mathjs.org/docs/reference/functions.html)\n\n2."p" represents count ranging from 1 to 200.\n\n3."[q]" represents the q(th) term (starts at 1).\n\n4."Example 1: 2*p = 0,2,4,8,10..."\n\n5.Example 2: [q-1]+[q-2]: Fibonacci Sequence.\n\n5.Nesting of [q[q-1]] must be avoided.\n\n6.Example 3: 2*sin(p*50)+tan(45): 1,1,1,0,0,0,0,0,0,0,1,1,2,2,3,3....'
+    text='1.Supports expressions containing *,^,/,%,sin,cos,tan and many more ...(https://mathjs.org/docs/reference/functions.html)\n\n2."p" represents count ranging from 1 to 200.\n\n3."[q]" represents the q(th) term (starts at 1).\n\n4."Example 1: 2*p = 0,2,4,6,8,10..."\n\n5.Example 2: [q-1]+[q-2]: Fibonacci Sequence.\n\n5.Nesting of [q[q-1]] must be avoided.\n\n6.Example 3: 2*sin(p*50)+tan(45): 1,1,1,0,0,0,0,0,0,0,1,1,2,2,3,3....'
     swal("Get Started !",text,'info')
 }
 
